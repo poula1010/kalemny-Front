51cf9bd81e24b2a915c0dc558b2e60c5acd48098
poula1010 <poula1010@hotmail.com>
2023-12-27 00:05:13 +0200

    02. main page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { TextBlockComponent } from './text-block/text-block.component';
 import { FormBlockComponent } from './form-block/form-block.component';
 import { SocketService } from './services/socket.service';
+import { TestpageComponent } from './testpage/testpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TextBlockComponent, FormBlockComponent],
+  imports: [CommonModule, RouterOutlet, TextBlockComponent, FormBlockComponent, TestpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/testpage/testpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-testpage',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './testpage.component.html',
+  styleUrl: './testpage.component.css'
+})
+export class TestpageComponent {
+
+}
